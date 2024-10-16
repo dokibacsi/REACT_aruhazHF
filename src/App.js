f6da72fb5ba5a0d72128c8def8a076527dd4e70d
dokibacsi <kvalcsi02@gmail.com>
2024-10-16 20:38:13 +0200

    aruhaz kosar feltolt plusz darab novel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Kosar from './components/kosar/Kosar';
+import Termekek from './components/termekek/Termekek';
+import { kosarLista, termekLista } from './datas';
+import { useState } from "react"
 
 function App() {
+  const [klist, setList] = useState(kosarLista)
+
+  function athelyez(termek) {
+    const ujKosarList = [...klist]
+    console.log(ujKosarList)
+    console.log(termek)
+    let hunci = 0
+    while(hunci < ujKosarList.length && ujKosarList[hunci].kategoria !== termek.kategoria){
+      hunci++
+    }
+    if(hunci < ujKosarList.length){
+      ujKosarList[hunci].db += 1
+    }else{
+      termek.db = 1
+      ujKosarList.push(termek)
+    }
+    setList(ujKosarList)
+
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Webáruház</h1>
       </header>
+      <main>
+        <div className="termekek">
+          <Termekek lista={termekLista} athelyez={athelyez} />
+        </div>
+        <div className="kosar">
+          <div className='elemek'>
+            <p>Termék</p>
+            <p>darab</p>
+            <p>ár</p>
+          </div>
+          <Kosar lista={klist} />
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/kosar/Kosar.js
@@ -0,0 +1,14 @@
+import React from "react"
+import KosarSor from "./KosarSor"
+
+export default function Kosar(props) {
+    return (
+        <>
+            {   
+                props.lista.map((elem, index) => {
+                    return(<KosarSor elem = {elem} key = {index} index = {index}/>)
+                })
+            }
+        </>
+    )
+}
